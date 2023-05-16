136c27928deac981b8bfa12766ff8bdf896ed034
SanjayB06 <sanjaymbharadwaj@gmail.com>
2023-05-16 10:38:51 -0700

    product service

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/firebase_data/ProductService.java
@@ -0,0 +1,54 @@
+package com.nighthawk.spring_portfolio.mvc.firebase_data;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
+//CRUD operations
+@Service
+public class ProductService {
+
+    public static final String COL_NAME="name";
+
+    public String saveProductDetails(Product product) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(product.getName()).set(product);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public Product getProductDetails(String name) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection(COL_NAME).document(name);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+
+        DocumentSnapshot document = future.get();
+
+        Product product = null;
+
+        if(document.exists()) {
+            product = document.toObject(Product.class);
+            return product;
+        }else {
+            return null;
+        }
+    }
+
+    public String updateProductDetails(Product product) throws InterruptedException, ExecutionException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(product.getName()).set(product);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String deleteProduct(String name) {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult = dbFirestore.collection(COL_NAME).document(name).delete();
+        return "Document with Patient ID "+name+" has been deleted";
+    }
+
+}
